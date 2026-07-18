21b372451cc5d842157cde8ee74073aeec8d0c17
Prathu <145953801+prathu-developer@users.noreply.github.com>
2026-07-18 18:49:05 +0530

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -12,7 +12,7 @@ from google.genai import types
 
 # --- DATABASE CONFIGURATION ---
 # We use environment variables so your password isn't exposed on GitHub
-DB_URL = os.environ.get("DATABASE_URL", "YOUR_SUPABASE_URL_HERE")
+DB_URL = os.environ.get("DATABASE_URL", "postgresql://postgres:FlUVu8dA8xy02woL@db.wuhoozvbufnwjsfpkojp.supabase.co:5432/postgres")
 
 # High-speed connection pool to handle massive group traffic instantly
 db_pool = psycopg2.pool.SimpleConnectionPool(1, 20, DB_URL)
